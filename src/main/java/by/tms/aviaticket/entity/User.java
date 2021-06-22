5ec00309f0c80e4c45b6732cf7dfba5405d36e70
tijani <viktorsee0@gmail.com>
2021-06-22 17:27:50 +0300

    add hibernate

--- src/main/java/by/tms/aviaticket/entity/User.java
+++ src/main/java/by/tms/aviaticket/entity/User.java
@@ -4,13 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import javax.validation.constraints.*;
 import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "users")
 public class User extends Passenger {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String email;
     private String phoneNumber;
